dlespiau/cc-oci-runtime

proxy: Add helpers for fd passing through an AF_UNIX socket
I'd like to give a fd to the client as part of the "allocateIO"
response.

Start by adding the necessary facilities to pass fds around.

Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

proxy/fdpassing.go
@@ -1,0 +1,68 @@
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"syscall"
+)
+
+// FD passing support.
+//
+// fds are passed through the out of band data of AF_UNIX sockets. Because it's
+// not possible to send out of band data without actual data to write, we write
+// a dummy byte, 'F', to the socket when passing the fd.
+
+var fileTag []byte = []byte{'F'}
+
+func WriteFd(c *net.UnixConn, fd int) error {
+	rights := syscall.UnixRights(fd)
+	_, _, err := c.WriteMsgUnix(fileTag, rights, nil)
+	return err
+}
+
+func ReadFd(c *net.UnixConn) (int, error) {
+	oob := make([]byte, 32)
+	buf := make([]byte, 1)
+
+	// Retrieve out of band data
+	n, oobn, _, _, err := c.ReadMsgUnix(buf, oob)
+	if oobn == 0 {
+		return -1, errors.New("no out of band data read")
+	}
+	if n != 1 && buf[0] != 'F' {
+		return -1, errors.New("couldn't read fd passing tag")
+	}
+
+	// Parse the fd out of the out of band data
+	scms, err := syscall.ParseSocketControlMessage(oob)
+	if err != nil {
+		return -1, err
+	}
+	if len(scms) != 1 {
+		return -1, fmt.Errorf("unexpected number of control messages (%d)", len(scms))
+	}
+	scm := scms[0]
+	fds, err := syscall.ParseUnixRights(&scm)
+	if err != nil {
+		return -1, err
+	}
+	if len(fds) != 1 {
+		return -1, fmt.Errorf("unexpected number of fds (%d)", len(fds))
+	}
+	return fds[0], nil
+}

proxy/fdpassing_test.go
@@ -1,0 +1,61 @@
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFdPassing(t *testing.T) {
+	reader, writer, err := os.Pipe()
+	assert.Nil(t, err)
+
+	// Passes the reader end of the pipe fd through a AF_UNIX connection
+	// and recreate an os.File from the received fd
+	c0, c1 := Socketpair(t)
+
+	err = WriteFd(c0, int(reader.Fd()))
+	assert.Nil(t, err)
+
+	newFd, err := ReadFd(c1)
+	assert.Nil(t, err)
+	assert.NotEqual(t, newFd, -1)
+
+	newReader := os.NewFile(uintptr(newFd), "")
+
+	// write into the pipe and check reading from newReader gives the
+	// expected result
+	var data []byte = []byte("foo")
+
+	n, err := writer.Write(data)
+	assert.Nil(t, err)
+	assert.Equal(t, n, len(data))
+
+	buf := make([]byte, 512)
+	n, err = newReader.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, n, len(data))
+	assert.Equal(t, data, buf[:n])
+
+	// cleanup
+	reader.Close()
+	writer.Close()
+	c0.Close()
+	c1.Close()
+	newReader.Close()
+}

clearcontainers/proxy

api/fdpassing.go
@@ -1,0 +1,71 @@
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"syscall"
+)
+
+// FD passing support.
+//
+// fds are passed through the out of band data of AF_UNIX sockets. Because it's
+// not possible to send out of band data without actual data to write, we write
+// a dummy byte, 'F', to the socket when passing the fd.
+
+var fileTag []byte = []byte{'F'}
+
+func WriteFd(c *net.UnixConn, fd int) error {
+	rights := syscall.UnixRights(fd)
+	_, _, err := c.WriteMsgUnix(fileTag, rights, nil)
+	return err
+}
+
+func ReadFd(c *net.UnixConn) (int, error) {
+	oob := make([]byte, 32)
+	buf := make([]byte, 1)
+
+	// Retrieve out of band data
+	n, oobn, _, _, err := c.ReadMsgUnix(buf, oob)
+	if err != nil {
+		return -1, err
+	}
+	if oobn == 0 {
+		return -1, errors.New("no out of band data read")
+	}
+	if n != 1 && buf[0] != 'F' {
+		return -1, errors.New("couldn't read fd passing tag")
+	}
+
+	// Parse the fd out of the out of band data
+	scms, err := syscall.ParseSocketControlMessage(oob)
+	if err != nil {
+		return -1, err
+	}
+	if len(scms) != 1 {
+		return -1, fmt.Errorf("unexpected number of control messages (%d)", len(scms))
+	}
+	scm := scms[0]
+	fds, err := syscall.ParseUnixRights(&scm)
+	if err != nil {
+		return -1, err
+	}
+	if len(fds) != 1 {
+		return -1, fmt.Errorf("unexpected number of fds (%d)", len(fds))
+	}
+	return fds[0], nil
+}

api/fdpassing_test.go
@@ -1,0 +1,62 @@
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFdPassing(t *testing.T) {
+	reader, writer, err := os.Pipe()
+	assert.Nil(t, err)
+
+	// Passes the reader end of the pipe fd through a AF_UNIX connection
+	// and recreate an os.File from the received fd
+	c0, c1, err := socketpair()
+	assert.Nil(t, err)
+
+	err = WriteFd(c0, int(reader.Fd()))
+	assert.Nil(t, err)
+
+	newFd, err := ReadFd(c1)
+	assert.Nil(t, err)
+	assert.NotEqual(t, newFd, -1)
+
+	newReader := os.NewFile(uintptr(newFd), "")
+
+	// write into the pipe and check reading from newReader gives the
+	// expected result
+	var data []byte = []byte("foo")
+
+	n, err := writer.Write(data)
+	assert.Nil(t, err)
+	assert.Equal(t, n, len(data))
+
+	buf := make([]byte, 512)
+	n, err = newReader.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, n, len(data))
+	assert.Equal(t, data, buf[:n])
+
+	// cleanup
+	reader.Close()
+	writer.Close()
+	c0.Close()
+	c1.Close()
+	newReader.Close()
+}
dlespiau/cc-oci-runtime

proxy: Add an attach command
Clients need to be able to attach a new connection to a running VM
(anything but $runtime create will need this). The attach payload can be
used to do just that.

Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

proxy/proxy.go
@@ -84,6 +84,29 @@ func helloHandler(data []byte, userData interface{}, response *HandlerResponse) 
 	client.vm = vm
 }
 
+// "attach"
+func attachHandler(data []byte, userData interface{}, response *HandlerResponse) {
+	client := userData.(*client)
+	proxy := client.proxy
+
+	attach := api.Attach{}
+	if err := json.Unmarshal(data, &attach); err != nil {
+		response.SetError(err)
+		return
+	}
+
+	proxy.Lock()
+	vm := proxy.vms[attach.ContainerId]
+	proxy.Unlock()
+
+	if vm == nil {
+		response.SetErrorf("unknown containerId: %s", attach.ContainerId)
+		return
+	}
+
+	client.vm = vm
+}
+
 // "bye"
 func byeHandler(data []byte, userData interface{}, response *HandlerResponse) {
 	client := userData.(*client)
@@ -213,6 +236,7 @@ func (proxy *proxy) Serve() {
 	// Define the client (runtime/shim) <-> proxy protocol
 	proto := NewProtocol()
 	proto.Handle("hello", helloHandler)
+	proto.Handle("attach", helloHandler)
 	proto.Handle("bye", byeHandler)
 	proto.Handle("allocateIO", allocateIoHandler)
 	proto.Handle("hyper", hyperHandler)

clearcontainers/proxy

proxy.go
@@ -84,6 +84,29 @@ func helloHandler(data []byte, userData interface{}, response *HandlerResponse) 
 	client.vm = vm
 }
 
+// "attach"
+func attachHandler(data []byte, userData interface{}, response *HandlerResponse) {
+	client := userData.(*client)
+	proxy := client.proxy
+
+	attach := api.Attach{}
+	if err := json.Unmarshal(data, &attach); err != nil {
+		response.SetError(err)
+		return
+	}
+
+	proxy.Lock()
+	vm := proxy.vms[attach.ContainerId]
+	proxy.Unlock()
+
+	if vm == nil {
+		response.SetErrorf("unknown containerId: %s", attach.ContainerId)
+		return
+	}
+
+	client.vm = vm
+}
+
 // "bye"
 func byeHandler(data []byte, userData interface{}, response *HandlerResponse) {
 	client := userData.(*client)
@@ -214,6 +237,7 @@ func (proxy *proxy) Serve() {
 	// Define the client (runtime/shim) <-> proxy protocol
 	proto := NewProtocol()
 	proto.Handle("hello", helloHandler)
+	proto.Handle("attach", attachHandler)
 	proto.Handle("bye", byeHandler)
 	proto.Handle("allocateIO", allocateIoHandler)
 	proto.Handle("hyper", hyperHandler)
f833c18d1ed18b3290535662bc789c7ff3df5ed9
Damien Lespiau <damien.lespiau@intel.com>
2017-05-27 17:43:39 +0100

    ci: Include coverage data of the 'client' package when running the main tests
    The main tests make heavy usage of the client package. The -coverpkg
    option allows to include more package than only the package currently
    tested in the coverage data.
    
    Including the client package shows we indeed cover quite a bit of it
    (80%) from the main tests.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- .ci/go-test.sh
+++ .ci/go-test.sh
@@ -3,7 +3,7 @@
 script_dir=$(cd `dirname $0`; pwd)
 root_dir=`dirname $script_dir`
 
-test_packages=". ./api"
+test_packages=('.;.,./client' './api;./api')
 go_test_flags="-v -race -timeout 2s"
 
 echo Running go test on packages "'$test_packages'" with flags "'$go_test_flags'"
@@ -19,8 +19,11 @@ function test_coverage
 {
 	echo "mode: atomic" > profile.cov
 
-	for pkg in $test_packages; do
-		go test $go_test_flags -covermode=atomic -coverprofile=profile_tmp.cov $pkg
+	for pkg in ${test_packages[@]}; do
+		fields=(${pkg//;/ })
+		pkg_name=${fields[0]}
+		pkg_cover=${fields[1]}
+		go test $go_test_flags -covermode=atomic -coverprofile=profile_tmp.cov -coverpkg $pkg_cover $pkg_name
 		[ -f profile_tmp.cov ] && tail -n +2 profile_tmp.cov >> profile.cov;
 		rm -f profile_tmp.cov
 	done
